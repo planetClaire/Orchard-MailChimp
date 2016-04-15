9bc36644dd20bc680d15d08d355cd48e5db71e72
marshy <marshy@zeta.com.au>
2016-04-15 13:16:39 +0800

    consolidate related resource class files

--- Resources/CampaignDefaults.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MailChimp.Resources {
-    public class CampaignDefaults
-    {
-        public string FromName { get; set; }
-        public string FromEmail { get; set; }
-        public string Subject { get; set; }
-        public string Language { get; set; }
-    }
-}
--- Resources/Member.cs
+++ Resources/Member.cs
@@ -30,7 +30,18 @@ namespace MailChimp.Resources
         public int AvgClickRate { get; set; }
     }
 
-    public enum EmailType {
+    public class Location
+    {
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        public int Gmtoff { get; set; }
+        public int Dstoff { get; set; }
+        public string CountryCode { get; set; }
+        public string Timezone { get; set; }
+    }
+
+    public enum EmailType
+    {
         Html,
         Text
     }
