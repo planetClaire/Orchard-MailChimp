d502c5054f440ab43f650d6e26860159220f038a
marshy <marshy@zeta.com.au>
2016-04-20 11:32:12 +0800

    begin tests

---
+++ Tests/MailChimpTests/Resources/MemberTests.cs
@@ -0,0 +1,43 @@
+﻿using MailChimp.Resources;
+using NUnit.Framework;
+
+namespace MailChimpTests.Resources
+{
+    [TestFixture]
+    public class MemberTests
+    {
+        [Test]
+        public void HasChangedSinceOptin_IfTimestampsDifferByMoreThan1Second_ReturnsTrue() {
+            var member = new Member {
+                TimestampOpt = "2015-09-16 19:24:27",
+                LastChanged = "2015-09-16 19:24:29"
+            };
+
+            Assert.True(member.HasChangedSinceOptin());
+        }
+
+        [Test]
+        public void HasChangedSinceOptin_IfTimestampsDifferBy1Second_ReturnsFalse()
+        {
+            var member = new Member
+            {
+                TimestampOpt = "2015-09-16 19:24:28",
+                LastChanged = "2015-09-16 19:24:29"
+            };
+
+            Assert.False(member.HasChangedSinceOptin());
+        }
+
+        [Test]
+        public void HasChangedSinceOptin_IfTimestampsEqual_ReturnsFalse()
+        {
+            var member = new Member
+            {
+                TimestampOpt = "2015-09-16 19:24:28",
+                LastChanged = "2015-09-16 19:24:28"
+            };
+
+            Assert.False(member.HasChangedSinceOptin());
+        }
+    }
+}
